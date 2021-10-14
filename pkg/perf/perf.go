49f12788aaddcc54c86426b7ae5dccf480945d86
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2021-10-14 23:02:46 +0800

    perf: Start parsing files created by JITs on Linux
    Parse the Linux jit-interface perf files[1] and sort by start
    addresses. There will be some dragons to make this work with
    containerized workloads (e.g. map the pid to nspid and then
    access /tmp/perf-${nspid}.map inside the right rootfs and be
    aware that this is entirely user controlled).
    
    [1] https://github.com/torvalds/linux/blob/master/tools/perf/Documentation/jit-interface.txt
    
    Signed-off-by: Holger Hans Peter Freyther <holger@moiji-mobile.com>

---
+++ pkg/perf/perf.go
@@ -0,0 +1,72 @@
+// Copyright 2021 The Parca Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package perf
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type PerfMapAddr struct {
+	Start  uint64
+	End    uint64
+	Symbol string
+}
+
+type PerfMap struct {
+	addrs []PerfMapAddr
+}
+
+func PerfReadMap(fs fs.FS, fileName string) (PerfMap, error) {
+	fd, err := fs.Open(fileName)
+	if err != nil {
+		return PerfMap{}, err
+	}
+	defer fd.Close()
+
+	s := bufio.NewScanner(fd)
+	addrs := make([]PerfMapAddr, 0)
+	for s.Scan() {
+		l := strings.SplitN(s.Text(), " ", 3)
+		if len(l) < 3 {
+			return PerfMap{}, fmt.Errorf("splitting failed: %v", l)
+
+		}
+
+		start, err := strconv.ParseUint(l[0], 16, 64)
+		if err != nil {
+			return PerfMap{}, fmt.Errorf("parsing start failed on %v: %w", l, err)
+		}
+		size, err := strconv.ParseUint(l[1], 16, 64)
+		if err != nil {
+			return PerfMap{}, fmt.Errorf("parsing end failed on %v: %w", l, err)
+		}
+		if start+size < start {
+			return PerfMap{}, fmt.Errorf("overflowed mapping: %v", l)
+		}
+		addrs = append(addrs, PerfMapAddr{start, start + size, l[2]})
+	}
+	// Sorted by end address to allow binary search during look-up. End to find
+	// the (closest) address _before_ the end. This could be an inlined instruction
+	// within a larger blob.
+	sort.Slice(addrs, func(i, j int) bool {
+		return addrs[i].End < addrs[j].End
+	})
+	return PerfMap{addrs: addrs}, s.Err()
+}
