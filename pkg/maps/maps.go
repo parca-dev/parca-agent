10b8ea5cdf91fc02bb01d4ff4c9bddb09c6d49f6
Kemal Akkoyun <kakkoyun@gmail.com>
2022-03-22 16:40:33 +0100

    Refactor code for readability
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- pkg/maps/maps.go
+++ pkg/maps/maps.go
@@ -92,7 +92,8 @@ func (c *PIDMappingFileCache) mappingForPID(pid uint32) ([]*profile.Mapping, err
 		// Try our best to have the BuildID.
 		if m.BuildID == "" {
 			// TODO(brancz): These need special cases. See pseudo-paths in proc's man page
-			if m.File == "[vdso]" || m.File == "[vsyscall]" || m.File == "[stack]" || m.File == "[heap]" || m.File == "" {
+			//  m.File == "[vdso]" || m.File == "[vsyscall]" || m.File == "[stack]" || m.File == "[heap]"
+			if m.Unsymbolizable() || m.File == "" {
 				continue
 			}
 
