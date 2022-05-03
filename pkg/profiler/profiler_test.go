eb55b662b9b43533168c9faed964a8c53959a400
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2022-05-03 03:55:40 -0700

    profiler: Add libbpfgo map tests
    To ensure that the libbpfgo invariants hold true. Adding these tests
    will also help us with library upgrades, as well as configuration
    changes in the way libbpf deals with errors, etc.
    
    **Test plan**:
    ```
    $ make test_profiler
    sudo -E GOOS=linux GOARCH=arm64 CC=clang CGO_CFLAGS="-I /home/vagrant/work/parca-agent/dist/libbpf/usr/include" CGO_LDFLAGS="/home/vagrant/work/parca-agent/dist/libbpf/libbpf.a" go test -v github.com/parca-dev/parca-agent/pkg/profiler
    === RUN   TestDeleteNonExistentKeyReturnsEnoent
    --- PASS: TestDeleteNonExistentKeyReturnsEnoent (0.00s)
    === RUN   TestDeleteExistentKey
    --- PASS: TestDeleteExistentKey (0.00s)
    === RUN   TestGetValueAndDeleteBatchWithEmptyMap
    --- PASS: TestGetValueAndDeleteBatchWithEmptyMap (0.00s)
    === RUN   TestGetValueAndDeleteBatchFewerElementsThanCount
    --- PASS: TestGetValueAndDeleteBatchFewerElementsThanCount (0.00s)
    === RUN   TestGetValueAndDeleteBatchExactElements
    --- PASS: TestGetValueAndDeleteBatchExactElements (0.00s)
    PASS
    ok      github.com/parca-dev/parca-agent/pkg/profiler   (cached)
    ```
    
    With ASAN:
    ```
    $ make test_profiler ENABLE_ASAN=yes
    sudo -E GOOS=linux GOARCH=arm64 CC=clang CGO_CFLAGS="-I /home/vagrant/work/parca-agent/dist/libbpf/usr/include" CGO_LDFLAGS="/home/vagrant/work/parca-agent/dist/libbpf/libbpf.a" go test -v github.com/parca-dev/parca-agent/pkg/profiler -asan
    === RUN   TestDeleteNonExistentKeyReturnsEnoent
    --- PASS: TestDeleteNonExistentKeyReturnsEnoent (0.00s)
    === RUN   TestDeleteExistentKey
    --- PASS: TestDeleteExistentKey (0.00s)
    === RUN   TestGetValueAndDeleteBatchWithEmptyMap
    --- PASS: TestGetValueAndDeleteBatchWithEmptyMap (0.00s)
    === RUN   TestGetValueAndDeleteBatchFewerElementsThanCount
    --- PASS: TestGetValueAndDeleteBatchFewerElementsThanCount (0.00s)
    === RUN   TestGetValueAndDeleteBatchExactElements
    --- PASS: TestGetValueAndDeleteBatchExactElements (0.00s)
    PASS
    ok      github.com/parca-dev/parca-agent/pkg/profiler   0.047s
    ```

---
+++ pkg/profiler/profiler_test.go
@@ -0,0 +1,141 @@
+// Copyright (c) 2022 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package profiler
+
+import (
+	"syscall"
+	"testing"
+	"unsafe"
+
+	bpf "github.com/aquasecurity/libbpfgo"
+
+	"github.com/stretchr/testify/require"
+)
+
+// The intent of these tests is to ensure that the BPF library we use,
+// (libbpfgo in this case) behaves in the way we expect.
+
+func SetUpBpfProgram(t *testing.T) (*bpf.Module, error) {
+	t.Helper()
+
+	m, err := bpf.NewModuleFromBufferArgs(bpf.NewModuleArgs{
+		BPFObjBuff: bpfObj,
+		BPFObjName: "parca",
+	})
+	require.NoError(t, err)
+
+	err = m.BPFLoadObject()
+	require.NoError(t, err)
+
+	return m, nil
+}
+
+func TestDeleteNonExistentKeyReturnsEnoent(t *testing.T) {
+	m, err := SetUpBpfProgram(t)
+	require.NoError(t, err)
+	t.Cleanup(m.Close)
+	bpfMap, err := m.GetMap("counts")
+	require.NoError(t, err)
+
+	stackID := int32(1234)
+
+	// Delete should fail as the key doesn't exist.
+	err = bpfMap.DeleteKey(unsafe.Pointer(&stackID))
+	require.Error(t, err)
+	require.ErrorIs(t, err, syscall.ENOENT)
+}
+
+func TestDeleteExistentKey(t *testing.T) {
+	m, err := SetUpBpfProgram(t)
+	require.NoError(t, err)
+	t.Cleanup(m.Close)
+	bpfMap, err := m.GetMap("counts")
+	require.NoError(t, err)
+
+	stackID := int32(1234)
+
+	// Insert some element that will be later deleted.
+	value := []byte{'a'}
+	err = bpfMap.Update(unsafe.Pointer(&stackID), unsafe.Pointer(&value[0]))
+	require.NoError(t, err)
+
+	// Delete should work.
+	err = bpfMap.DeleteKey(unsafe.Pointer(&stackID))
+	require.NoError(t, err)
+}
+
+func TestGetValueAndDeleteBatchWithEmptyMap(t *testing.T) {
+	m, err := SetUpBpfProgram(t)
+	require.NoError(t, err)
+	t.Cleanup(m.Close)
+	bpfMap, err := m.GetMap("counts")
+	require.NoError(t, err)
+
+	keys := make([]stackCountKey, bpfMap.GetMaxEntries())
+	countKeysPtr := unsafe.Pointer(&keys[0])
+	nextCountKey := uintptr(1)
+	batchSize := bpfMap.GetMaxEntries()
+	values, err := bpfMap.GetValueAndDeleteBatch(countKeysPtr, nil, unsafe.Pointer(&nextCountKey), batchSize)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(values))
+}
+
+func TestGetValueAndDeleteBatchFewerElementsThanCount(t *testing.T) {
+	m, err := SetUpBpfProgram(t)
+	require.NoError(t, err)
+	t.Cleanup(m.Close)
+	bpfMap, err := m.GetMap("counts")
+	require.NoError(t, err)
+
+	stackID := int32(1234)
+
+	// Insert some element that will be later deleted.
+	value := []byte{'a'}
+	err = bpfMap.Update(unsafe.Pointer(&stackID), unsafe.Pointer(&value[0]))
+	require.NoError(t, err)
+
+	// Request more elements than we have, this should return and delete everything.
+	keys := make([]stackCountKey, bpfMap.GetMaxEntries())
+	countKeysPtr := unsafe.Pointer(&keys[0])
+	nextCountKey := uintptr(1)
+	batchSize := bpfMap.GetMaxEntries()
+	values, err := bpfMap.GetValueAndDeleteBatch(countKeysPtr, nil, unsafe.Pointer(&nextCountKey), batchSize)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(values))
+}
+
+func TestGetValueAndDeleteBatchExactElements(t *testing.T) {
+	m, err := SetUpBpfProgram(t)
+	require.NoError(t, err)
+	t.Cleanup(m.Close)
+	bpfMap, err := m.GetMap("counts")
+	require.NoError(t, err)
+
+	stackID := int32(1234)
+
+	// Insert some element that will be later deleted.
+	value := []byte{'a'}
+	err = bpfMap.Update(unsafe.Pointer(&stackID), unsafe.Pointer(&value[0]))
+	require.NoError(t, err)
+
+	// Request exactly the elements we have.
+	keys := make([]stackCountKey, 1)
+	countKeysPtr := unsafe.Pointer(&keys[0])
+	nextCountKey := uintptr(1)
+	batchSize := uint32(1)
+	values, err := bpfMap.GetValueAndDeleteBatch(countKeysPtr, nil, unsafe.Pointer(&nextCountKey), batchSize)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(values))
+}
