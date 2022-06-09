7a5f964d58b49d37ec86f1f4494015c169eca340
Maxime Brunet <max@brnt.mx>
2022-06-08 18:47:13 -0700

    chore: add shell.nix file for development

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell rec {
+  name = "parca-agent";
+
+  packages = with pkgs; [
+    clang
+    gnumake
+    go_1_18
+    kubectl
+    libbpf
+    libelf
+    llvm
+    minikube
+    nodejs-16_x
+    rustup
+    zlib.static
+  ] ++ (lib.optional stdenv.isLinux [ glibc glibc.static ]);
+
+  shellHook = ''
+    export PATH="''${PROJECT_ROOT}/bin:''${CARGO_HOME}/bin:''${PATH}"
+    rustup show >/dev/null
+  '';
+
+  PROJECT_ROOT = builtins.toString ./.;
+  CARGO_HOME = "${PROJECT_ROOT}/tmp/cargo";
+  GOBIN = "${PROJECT_ROOT}/bin";
+  RUSTUP_HOME = "${PROJECT_ROOT}/tmp/rustup";
+  RUST_BACKTRACE = 1;
+}
+
