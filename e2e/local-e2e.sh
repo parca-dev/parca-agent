5799e29a2b97f0a06c3254749e6d3da4d048c529
Sumera Priyadarsini <sylphrenadin@gmail.com>
2022-06-20 10:41:36 +0530

    [WIP]ci: Add smoke tests to ci

--- e2e/local-e2e.sh
+++ e2e/local-e2e.sh
@@ -28,11 +28,11 @@ function minikube_up(){
     --insecure-registry="localhost:5000"\
     --driver=$DRIVER \
     --kubernetes-version=v1.22.3 \
-    --cpus=4 \
-    --memory=16gb \
-    --disk-size=20gb \
-    --docker-opt dns=8.8.8.8 \
-    --docker-opt default-ulimit=memlock=9223372036854775807:9223372036854775807
+    #--cpus=4 \
+    #--memory=16gb \
+   # --disk-size=20gb \
+    #--docker-opt dns=8.8.8.8 \
+    #--docker-opt default-ulimit=memlock=9223372036854775807:9223372036854775807
 
   eval $(minikube -p parca-e2e docker-env)
 }
