775353b39189e5e74ba46f29c5195722eaefcda7
Maxime Brunet <max@brnt.mx>
2022-07-24 23:40:50 -0700

    chore(nix-shell): fix llvm (#468)
    * chore: fix llvm in nix-shell
    
    * Install bpf-linker respecting lock file

--- shell.nix
+++ shell.nix
@@ -4,12 +4,13 @@ pkgs.mkShell rec {
   name = "parca-agent";
 
   packages = with pkgs; [
-    clang
+    clang_14
     elfutils.dev
     gnumake
     go_1_18
     kubectl
-    llvm
+    libxml2.dev
+    llvmPackages_14.llvm
     minikube
     pkg-config
     rustup
@@ -26,5 +27,10 @@ pkgs.mkShell rec {
   GOBIN = "${PROJECT_ROOT}/bin";
   RUSTUP_HOME = "${PROJECT_ROOT}/tmp/rustup";
   RUST_BACKTRACE = 1;
+
+  # we need to do this as we're using an external LLVM. See: https://github.com/aya-rs/bpf-linker#using-external-llvm
+  # TODO(vadorovsky): Remove the LLVM_SYS_140_PREFIX variable once
+  # https://gitlab.com/taricorp/llvm-sys.rs/-/merge_requests/22 is merged.
+  LLVM_SYS_140_PREFIX = "${pkgs.llvmPackages_14.llvm.dev}";
 }
 
