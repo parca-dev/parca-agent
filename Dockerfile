e1614a24eb906acbf5e4fb8f8d287566be771765
Maxime Brunet <max@brnt.mx>
2022-08-22 21:25:52 -0700

    ci: set up pre-commit (#723)
    * ci: set up pre-commit
    
    * typos

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+# https://github.com/hadolint/hadolint/issues/861
+# hadolint ignore=DL3029
 FROM --platform="${BUILDPLATFORM:-linux/amd64}" docker.io/library/busybox:1.35.0@sha256:09439c073bd3eb029a91c72eff2c0d9f12ab9c84f66bdef360fcf3f91a81bf7c as builder
 RUN mkdir /.cache && touch -t 202101010000.00 /.cache
 
@@ -9,15 +11,16 @@ WORKDIR /app
 COPY goreleaser/dist dist
 
 # NOTICE: See goreleaser.yml for the build paths.
-RUN if [ "${TARGETARCH}" == 'amd64' ]; then \
+RUN if [ "${TARGETARCH}" = 'amd64' ]; then \
         cp "dist/parca-agent-${TARGETARCH}_${TARGETOS}_${TARGETARCH}_${TARGETVARIANT:-v1}/parca-agent" . ; \
-    elif [ "${TARGETARCH}" == 'arm' ]; then \
+    elif [ "${TARGETARCH}" = 'arm' ]; then \
         cp "dist/parca-agent-${TARGETARCH}_${TARGETOS}_${TARGETARCH}_${TARGETVARIANT##v}/parca-agent" . ; \
     else \
         cp "dist/parca-agent-${TARGETARCH}_${TARGETOS}_${TARGETARCH}/parca-agent" . ; \
     fi
 RUN chmod +x parca-agent
 
+# hadolint ignore=DL3029
 FROM --platform="${TARGETPLATFORM:-linux/amd64}" gcr.io/distroless/static@sha256:35817ff4e3441ebfa430cfca98121e43cbf4a134412285c2460a9464c97ac2ca
 COPY --chown=0:0 --from=builder /app/parca-agent /bin/parca-agent
 CMD ["/bin/parca-agent"]
--- e2e/local-e2e.sh
+++ e2e/local-e2e.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Copyright 2022 The Parca Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ################################################################################
 #
 # This script is meant to be run from the root of this project
@@ -9,105 +22,89 @@
 set -euo pipefail
 
 function run() {
-  # Driver set to virtualbox by default if not specified
-  DRIVER=${1-virtualbox}
+    # Driver set to virtualbox by default if not specified
+    DRIVER=${1-virtualbox}
 
-  minikube_up $DRIVER
-  generate_manifests
-  deploy
+    minikube_up "${DRIVER}"
+    generate_manifests
+    deploy
 }
 
 # Create local minikube cluster and deploys the dev env for parca and parca agent
-function minikube_up(){
-  # can be virtualbox, vmwarefusion, kvm2, vmware, none, docker, podman, ssh
-  DRIVER=$1
+function minikube_up() {
+    # can be virtualbox, vmwarefusion, kvm2, vmware, none, docker, podman, ssh
+    DRIVER=$1
 
-  echo "Spinning up a parca dev cluster"
-  minikube start -p parca-e2e \
-    --container-runtime=docker \
-    --insecure-registry="localhost:5000"\
-    --driver=$DRIVER \
-    --kubernetes-version=v1.22.3 \
-    --cpus=4 \
-    --memory=16gb \
-    --disk-size=20gb \
-    --docker-opt dns=8.8.8.8 \
-    --docker-opt default-ulimit=memlock=9223372036854775807:9223372036854775807
+    echo "Spinning up a parca dev cluster"
+    minikube start -p parca-e2e \
+        --container-runtime=docker \
+        --insecure-registry="localhost:5000" \
+        --driver="${DRIVER}" \
+        --kubernetes-version=v1.22.3 \
+        --cpus=4 \
+        --memory=16gb \
+        --disk-size=20gb \
+        --docker-opt dns=8.8.8.8 \
+        --docker-opt default-ulimit=memlock=9223372036854775807:9223372036854775807
 
-  eval $(minikube -p parca-e2e docker-env)
+    eval "$(minikube -p parca-e2e docker-env)"
 }
 
 # Delete minikube instance
-function minikube_down(){
-  echo "Deleting parca-e2e cluster"
-  minikube delete -p parca-e2e
+function minikube_down() {
+    echo "Deleting parca-e2e cluster"
+    minikube delete -p parca-e2e
 }
 
 # Configure clusters to run latest commit in Parca agent
 function deploy() {
-  SERVER_LATEST_VERSION=$(curl -s https://api.github.com/repos/parca-dev/parca/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | xargs echo -n)
-  echo "Server version: $SERVER_LATEST_VERSION"
+    SERVER_LATEST_VERSION=$(curl -s https://api.github.com/repos/parca-dev/parca/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | xargs echo -n)
+    echo "Server version: $SERVER_LATEST_VERSION"
 
-  if ! check_ns_parca; then
-    kubectl create namespace parca
-  fi
+    if ! check_ns_parca; then
+        kubectl create namespace parca
+    fi
 
-  kubectl apply -f https://github.com/parca-dev/parca/releases/download/"$SERVER_LATEST_VERSION"/kubernetes-manifest.yaml
-  kubectl -n parca rollout status deployment/parca --timeout=2m
+    kubectl apply -f https://github.com/parca-dev/parca/releases/download/"$SERVER_LATEST_VERSION"/kubernetes-manifest.yaml
+    kubectl -n parca rollout status deployment/parca --timeout=2m
 
-  kubectl apply -f ./manifests/local/manifest-e2e.yaml
-  kubectl -n parca rollout status daemonset/parca-agent --timeout=2m
+    kubectl apply -f ./manifests/local/manifest-e2e.yaml
+    kubectl -n parca rollout status daemonset/parca-agent --timeout=2m
 
-  echo "Connecting to Parca and Parca agent"
+    echo "Connecting to Parca and Parca agent"
 
-  kubectl port-forward -n parca service/parca 7070 &
-  kubectl port-forward -n parca $(kubectl get po -n parca | grep parca-agent | awk '{print $1;}') 7071:7071 &
+    kubectl port-forward -n parca service/parca 7070 &
+    kubectl port-forward -n parca daemonset/parca-agent 7071:7071 &
 }
 
 function vendor() {
-  jb install
+    jb install
 }
 
 function check_ns_parca() {
- ns_status=$(kubectl get ns parca)
- if [ $? -ne 0 ]; then
-   return 1
- fi
- echo "namespace parca already present"
+    if ! kubectl get ns parca >/dev/null; then
+        return 1
+    fi
+    echo "namespace parca already present"
 }
 
 # Build image with latest commit
 function generate_manifests() {
-  if [ -z ${GITHUB_BRANCH_NAME+x} ]; then
-    BRANCH=$(git rev-parse --abbrev-ref HEAD)-
-  else
-    BRANCH=$(GITHUB_BRANCH_NAME)
-  fi
+    BRANCH=${GITHUB_BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)-}
 
-  if [ -z ${GITHUB_SHA+x} ]; then
-    COMMIT=$(git describe --no-match --dirty --always --abbrev=8)
-  else
-    COMMIT=$(echo $(GITHUB_SHA) | cut -c1-8)
-  fi
+    if [[ -z "${GITHUB_SHA:-}" ]]; then
+        COMMIT=$(git describe --no-match --dirty --always --abbrev=8)
+    else
+        COMMIT=${GITHUB_SHA:0:8}
+    fi
 
-  if [ -z ${RELEASE_TAG+x} ]; then
-    VERSION=$(git describe --tags 2>/dev/null || echo '$(BRANCH)$(COMMIT)')
-  else
-    VERSION=$RELEASE_TAG
-  fi
+    VERSION=${RELEASE_TAG:-$(git describe --tags 2>/dev/null || echo "${BRANCH}${COMMIT}")}
 
-  rm -rf manifests/local
-  mkdir -p manifests/local
+    rm -rf manifests/local
+    mkdir -p manifests/local
 
-  jsonnet --tla-str version="$VERSION" -J vendor e2e.jsonnet -m manifests/local | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml; rm -f {}'
-  awk 'BEGINFILE {print "---"}{print}' manifests/local/* > manifests/local/manifest-e2e.yaml
+    jsonnet --tla-str version="$VERSION" -J vendor e2e.jsonnet -m manifests/local | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml; rm -f {}'
+    awk 'BEGINFILE {print "---"}{print}' manifests/local/* >manifests/local/manifest-e2e.yaml
 
-  GOLANG_BASE=golang:1.18.3-bullseye
-  DEBIAN_BASE=debian:bullseye-slim
-  LINUX_ARCH=$(uname -p)
-
-  docker build -t localhost:5000/parca-agent:"$VERSION" \
-     --build-arg GOLANG_BASE="$GOLANG_BASE" \
-     --build-arg DEBIAN_BASE="$DEBIAN_BASE"  \
-     --build-arg LINUX_ARCH=$LINUX_ARCH  ./..
+    docker build -t localhost:5000/parca-agent:"$VERSION" ./..
 }
