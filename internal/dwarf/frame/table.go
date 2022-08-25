2cee06d93bc14196b5ac36c1e8c5471baeacdd00
Vaishali Thakkar <me.vaishalithakkar@gmail.com>
2022-08-25 19:26:10 +0200

    Remove duplicate function call and push readelf output
    Signed-off-by: Vaishali Thakkar <me.vaishalithakkar@gmail.com>

--- internal/dwarf/frame/table.go
+++ internal/dwarf/frame/table.go
@@ -230,7 +230,6 @@ func executeDwarfProgramUntilPC(fde *DescriptionEntry, pc uint64) *FrameContext 
 	frameContext.order = fde.order
 	frame.loc = fde.Begin()
 	frame.address = pc
-	//frameContext.ExecuteUntilPC(fde.Instructions)
 
 	return frameContext
 }
