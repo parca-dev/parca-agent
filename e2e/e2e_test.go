c5b020e4ec572a787e77427b88006fcb7d3a5543
Sumera <35404119+Sylfrena@users.noreply.github.com>
2022-10-17 21:23:41 +0530

    ci: Enable e2e testing in ci (#478)
    * ci : Enable e2e testing
    
    Modify actions and tests to allow e2e testing in parca agent.
    
    For every PR, a test image of parca agent is created(hosted at parca-agent-test)
    which is then tested alongwith the latest release of parca server
    within a minikube setup in CI.
    
    Add support for debugging opaque failures in CI using upterm shell.
    
    All scripts and tests can be found in the `e2e` directory in parca-agent
    
    Co-developed-by: Frederic Branczyk
    Signed-off-by: Sumera Priyadarsini <sylphrenadin@gmail.com>
    
    * Add jq and cleanup
    
    Signed-off-by: Sumera Priyadarsini <sylphrenadin@gmail.com>
    
    * Use parca-agent-test version
    
    Signed-off-by: Sumera Priyadarsini <sylphrenadin@gmail.com>
    
    Signed-off-by: Sumera Priyadarsini <sylphrenadin@gmail.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -15,34 +15,124 @@ package e2e
 
 import (
 	"context"
+	"flag"
+	"math"
 	"testing"
+	"time"
 
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
 
 	pb "github.com/parca-dev/parca/gen/proto/go/parca/query/v1alpha1"
 )
 
-func TestIntegrationGRPC(t *testing.T) {
+var kubeconfig = flag.String("kubeconfig", "~/.kube/config", "kube config path")
+
+// Checks for parca-server and parca-agent pods and returns pod names if true
+// Returns empty string if no pods are found.
+func CheckPodsExist(t *testing.T, ctx context.Context, kubeClient kubernetes.Interface) (string, string) {
+	t.Helper()
+
+	labelSelectorParcaServer := labels.FormatLabels(map[string]string{"app.kubernetes.io/name": "parca"})
+	labelSelectorParcaAgent := labels.FormatLabels(map[string]string{"app.kubernetes.io/name": "parca-agent"})
+
+	parcaServerPod, err := kubeClient.CoreV1().Pods("parca").List(ctx, metav1.ListOptions{LabelSelector: labelSelectorParcaServer})
+	require.NoErrorf(t, err, "Unable to fetch pods in parca namespace: %w", err)
+
+	parcaAgentPod, err := kubeClient.CoreV1().Pods("parca").List(ctx, metav1.ListOptions{LabelSelector: labelSelectorParcaAgent})
+	require.NoErrorf(t, err, "Unable to fetch pods in parca namespace: %w", err)
+
+	if len(parcaServerPod.Items) == 0 {
+		t.Log("Parca Server Pod not found")
+		return "", ""
+	}
+
+	if len(parcaAgentPod.Items) == 0 {
+		t.Log("Parca Agent Pod not found")
+		return "", ""
+	}
+
+	return parcaServerPod.Items[0].Name, parcaAgentPod.Items[0].Name
+}
+
+// TODO(sylfrena): Cleanup logs once e2e tests are stabilized
+// TODO(sylfrena): Reduce context timeouts
+// TODO(sylfrena): Use exponential backoff instead.
+func TestGRPCIntegration(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	cfg, err := GetKubeConfig(*kubeconfig)
+	require.NoError(t, err)
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	require.NoError(t, err)
+
+	parcaServer, parcaAgent := CheckPodsExist(t, ctx, kubeClient)
+	t.Log("Pods discovered: ", parcaServer, parcaAgent)
+
+	ns := "parca"
+
+	serverCloser, err := StartPortForward(ctx, cfg, "https", parcaServer, ns, "7070")
+	if err != nil {
+		require.NoError(t, err, "failed to start port forwarding Parca Server: %v", err)
+	}
+	defer serverCloser()
+
+	agentCloser, err := StartPortForward(ctx, cfg, "https", parcaAgent, ns, "7071")
+	if err != nil {
+		require.NoError(t, err, "failed to start port forwarding Parca Agent: %v", err)
+	}
+	defer agentCloser()
+
+	t.Log("Starting tests")
 	conn, err := grpc.Dial("127.0.0.1:7070", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	require.NoError(t, err)
 	defer conn.Close()
 
+	t.Log("Creating query service client")
 	c := pb.NewQueryServiceClient(conn)
-	ctx := context.Background()
 
-	queryRequest := &pb.QueryRangeRequest{
-		Query: `parca_agent_cpu:samples:count:cpu:nanoseconds:delta{container="parca-agent"}`,
+	t.Log("Performing Query Range Request")
+	queryRequestAgent := &pb.QueryRangeRequest{
+		Query: `parca_agent_cpu:samples:count:cpu:nanoseconds:delta`,
 		Start: timestamppb.New(timestamp.Time(0)),
-		End:   timestamppb.New(timestamp.Time(9223372036854775807)),
+		End:   timestamppb.New(timestamp.Time(math.MaxInt64)),
 		Limit: 10,
 	}
 
-	resp, err := c.QueryRange(ctx, queryRequest)
+	for i := 0; i < 10; i++ {
+		ctx, cancel = context.WithTimeout(context.Background(), 1*time.Minute)
+		defer cancel()
+		resp, err := c.QueryRange(ctx, queryRequestAgent)
+		if err != nil {
+			status, ok := status.FromError(err)
+			if ok && status.Code() == codes.Unavailable {
+				t.Log("query range api unavailable, retrying in a second")
+				time.Sleep(time.Minute)
+				continue
+			}
+			if ok && status.Code() == codes.NotFound {
+				t.Log("query range resource not found, retrying in a minute\n", err)
+				time.Sleep(time.Minute)
+				continue
+			}
+			if ok && status.Code() == codes.DeadlineExceeded {
+				t.Log("deadline exceeded\n", err)
+				time.Sleep(time.Minute)
+				continue
+			}
+			t.Error(err)
+		}
 
-	require.NoError(t, err)
-	require.NotEmpty(t, resp.Series)
+		require.NotEmpty(t, resp.Series)
+	}
 }
