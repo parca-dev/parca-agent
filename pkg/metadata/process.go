c9021456bab3aa2a66b2c390b46b4bbe39e67849
Maxime Brunet <max@brnt.mx>
2022-11-29 06:03:19 -0800

    refactor(metadata): consolidate cgroup/process into procfs provider (#1024)
    * refactor(metadata): consolidate cgroup/process into procfs provider
    
    * Fix cgroup condition
    
    * Return cgroup when found
    
    * Rename back to Process
    
    * Update pkg/metadata/process.go
    
    Co-authored-by: Kemal Akkoyun <kakkoyun@users.noreply.github.com>
    
    * Rename func to findFirstCPUCgroup
    
    * Add comments and fix systemd slice condition
    
    Co-authored-by: Kemal Akkoyun <kakkoyun@users.noreply.github.com>

--- pkg/metadata/process.go
+++ pkg/metadata/process.go
@@ -15,7 +15,9 @@
 package metadata
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/procfs"
@@ -25,28 +27,68 @@ func Process() Provider {
 	return &StatelessProvider{"process", func(pid int) (model.LabelSet, error) {
 		p, err := procfs.NewProc(pid)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to instantiate procfs for PID %d: %w", pid, err)
 		}
+
+		cgroups, err := p.Cgroups()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get cgroups for PID %d: %w", pid, err)
+		}
+
+		cgroup := findFirstCPUCgroup(cgroups)
 
 		comm, err := p.Comm()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to get comm for PID %d: %w", pid, err)
 		}
 
 		executable, err := p.Executable()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to get executable for PID %d: %w", pid, err)
 		}
 
 		stat, err := p.Stat()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to get stat for PID %d: %w", pid, err)
 		}
 
 		return model.LabelSet{
-			"comm":       model.LabelValue(comm),
-			"executable": model.LabelValue(executable),
-			"ppid":       model.LabelValue(strconv.Itoa(stat.PPID)),
+			"cgroup_name": model.LabelValue(cgroup.Path),
+			"comm":        model.LabelValue(comm),
+			"executable":  model.LabelValue(executable),
+			"ppid":        model.LabelValue(strconv.Itoa(stat.PPID)),
 		}, nil
 	}}
+}
+
+func findFirstCPUCgroup(cgroups []procfs.Cgroup) procfs.Cgroup {
+	// If only 1 cgroup, simply return it
+	if len(cgroups) == 1 {
+		return cgroups[0]
+	}
+
+	for _, cg := range cgroups {
+		// Find first cgroup v1 with cpu controller
+		for _, ctlr := range cg.Controllers {
+			if ctlr == "cpu" {
+				return cg
+			}
+		}
+
+		// Find first systemd slice
+		// https://systemd.io/CGROUP_DELEGATION/#systemds-unit-types
+		if strings.HasPrefix(cg.Path, "/system.slice/") || strings.HasPrefix(cg.Path, "/user.slice/") {
+			return cg
+		}
+
+		// FIXME: what are we looking for here?
+		// https://systemd.io/CGROUP_DELEGATION/#controller-support
+		for _, ctlr := range cg.Controllers {
+			if strings.Contains(ctlr, "systemd") {
+				return cg
+			}
+		}
+	}
+
+	return procfs.Cgroup{}
 }
