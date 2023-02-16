652addfdcad55ed514276ca05380f94287b070e1
杨杰 <zdyj3170101136@gmail.com>
2023-02-16 18:19:49 +0800

    pkg/debuginfo: add plt symbol

--- pkg/debuginfo/extract.go
+++ pkg/debuginfo/extract.go
@@ -86,6 +86,7 @@ func (e *Extractor) Extract(ctx context.Context, dst io.WriteSeeker, src string)
 		isDwarf,
 		isSymbolTable,
 		isGoSymbolTable,
+		isPltSymbolTable,
 		func(s *elf.Section) bool {
 			return s.Type == elf.SHT_NOTE
 		})
@@ -119,4 +120,8 @@ var isSymbolTable = func(s *elf.Section) bool {
 
 var isGoSymbolTable = func(s *elf.Section) bool {
 	return s.Name == ".gosymtab" || s.Name == ".gopclntab" || s.Name == ".go.buildinfo"
+}
+
+var isPltSymbolTable = func(s *elf.Section) bool {
+	return s.Name == ".rela.plt" || s.Name == ".plt"
 }
