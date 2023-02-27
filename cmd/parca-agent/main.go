ea6cff4ec1eb4b7cc03c152216d973191f70c28d
Vaishali Thakkar <me.vaishalithakkar@gmail.com>
2023-02-27 06:11:23 +0100

    Discover java processes via hsperfdata
    Java Hotspot VM provides telemetry data through jvmstat
    performace counters and publishes itthrough a memory-mapped
    file in a temp directory /tmp/hsperfdata_{user}/{pid}. Currently
    in parca-agent there is no way to determine if a certain
    process is a Java process. So above naming convention will
    help us to find running Java processes in the system as well.
    
    This commit adds a package to find java processes on a host
    as well as in containers. It also adds a new metadata label
    'java' which follows 'java:true' format.
    
    Signed-off-by: Vaishali Thakkar <me.vaishalithakkar@gmail.com>

--- cmd/parca-agent/main.go
+++ cmd/parca-agent/main.go
@@ -422,6 +422,7 @@ func run(logger log.Logger, reg *prometheus.Registry, flags flags) error {
 			metadata.Target(flags.Node, flags.MetadataExternalLabels),
 			metadata.Compiler(),
 			metadata.Process(pfs),
+			metadata.JavaProcess(logger),
 			metadata.System(),
 			metadata.PodHosts(),
 		},
