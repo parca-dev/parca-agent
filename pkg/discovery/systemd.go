bb304d67f982bfc504d819c3e98bfb25f7fd0a31
marselester <marselester@gmail.com>
2023-03-07 20:39:07 -0500

    Draft of systemd discoverer with less allocs
    The current version makes use of 1163552 B/op, 32332 allocs/op
    to get a list of systemd services along with their PIDs.
    That accounts for 40% of CPU cycles, excluding the GC time.
    The proposed change gets 14304 B/op, 30 allocs/op.

--- pkg/discovery/systemd.go
+++ pkg/discovery/systemd.go
@@ -22,6 +22,7 @@ import (
 	systemd "github.com/coreos/go-systemd/v22/dbus"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
+	systemd2 "github.com/marselester/systemd"
 	"github.com/prometheus/common/model"
 )
 
@@ -106,4 +107,126 @@ func (c *SystemdDiscoverer) Run(ctx context.Context, up chan<- []*Group) error {
 			return ctx.Err()
 		}
 	}
+}
+
+type Systemd2Config struct{}
+
+func NewSystemd2Config() *Systemd2Config {
+	return &Systemd2Config{}
+}
+
+func (c *Systemd2Config) Name() string {
+	return "systemd2"
+}
+
+func (c *Systemd2Config) NewDiscoverer(d DiscovererOptions) (Discoverer, error) {
+	return &Systemd2Discoverer{
+		logger: d.Logger,
+		prev:   make(map[string]systemd2.Unit),
+	}, nil
+}
+
+type Systemd2Discoverer struct {
+	logger log.Logger
+	prev   map[string]systemd2.Unit
+}
+
+func (d *Systemd2Discoverer) Run(ctx context.Context, up chan<- []*Group) error {
+	c, err := systemd2.New()
+	if err != nil {
+		return fmt.Errorf("failed to connect to systemd D-Bus API, %w", err)
+	}
+	defer func() {
+		if err := c.Close(); err != nil {
+			level.Warn(d.logger).Log("msg", "failed to close systemd", "err", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			update, err := d.updatedUnits(c)
+			if err != nil {
+				level.Warn(d.logger).Log("msg", "failed to get units from systemd D-Bus API", "err", err)
+				continue
+			}
+			if len(update) == 0 {
+				continue
+			}
+
+			groups := make([]*Group, 0, len(update))
+			for unitName, unit := range update {
+				if unit.Name == "" || unit.SubState != "running" {
+					groups = append(groups, &Group{Source: unitName})
+					continue
+				}
+
+				pid, err := c.MainPID(unitName)
+				if err != nil {
+					level.Warn(d.logger).Log("msg", "failed to get MainPID from systemd D-Bus API", "err", err, "unit", unitName)
+					continue
+				}
+
+				groups = append(groups, &Group{
+					Targets: []model.LabelSet{{}},
+					Labels: model.LabelSet{
+						model.LabelName("systemd_unit"): model.LabelValue(unitName),
+					},
+					Source:   unitName,
+					EntryPID: int(pid),
+				})
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case up <- groups:
+			}
+
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+// updatedUnits is like SubscribeUnitsCustom
+// from github.com/coreos/go-systemd/v22/dbus,
+// i.e., it returns systemd units if there were any changes detected.
+func (d *Systemd2Discoverer) updatedUnits(c *systemd2.Client) (map[string]systemd2.Unit, error) {
+	cur := make(map[string]systemd2.Unit)
+	err := c.ListUnits(systemd2.IsService, func(u *systemd2.Unit) {
+		// Must copy a unit,
+		// otherwise it will be modified on the next function call.
+		cur[u.Name] = *u
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Collect all new and changed units.
+	changed := make(map[string]systemd2.Unit)
+	for name, unit := range cur {
+		prevUnit, ok := d.prev[name]
+		// Is it a new unit or
+		// the existing one but with an updated substate?
+		if !ok || prevUnit.SubState != unit.SubState {
+			changed[name] = unit
+		}
+
+		delete(d.prev, name)
+	}
+
+	// Add all deleted units.
+	for name := range d.prev {
+		changed[name] = systemd2.Unit{}
+	}
+
+	d.prev = cur
+
+	// No changes.
+	if len(changed) == 0 {
+		return nil, nil //nolint:nilnil
+	}
+
+	return changed, nil
 }
