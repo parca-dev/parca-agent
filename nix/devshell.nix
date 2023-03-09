6726e802d8579866202a47b5e99def26f3344511
Maxime Brunet <max@brnt.mx>
2023-03-08 23:18:55 -0800

    chore: migrate to `nix develop` (#1422)

---
+++ nix/devshell.nix
@@ -0,0 +1,37 @@
+{ pkgs }:
+
+let
+  goTools = pkgs.callPackage (import ./go-tools.nix) { };
+in
+(pkgs.mkShell.override {
+  inherit (pkgs.llvmPackages_14) stdenv;
+}) rec {
+  name = "parca-agent-devshell";
+
+  packages = with pkgs; [
+    bpftools
+    docker-machine-kvm2
+    elfutils.dev
+    glibc.dev
+    glibc.static
+    go-jsonnet
+    goTools.bluebox
+    goTools.embedmd
+    go_1_20
+    gofumpt
+    gojsontoyaml
+    # Build with Go 1.20
+    # https://github.com/golangci/golangci-lint/issues/3565
+    (golangci-lint.override {
+      buildGoModule = buildGo120Module;
+    })
+    jsonnet-bundler
+    kubectl
+    llvm_14
+    minikube
+    pkg-config
+    pre-commit
+    tilt
+    zlib.static
+  ];
+}
--- shell.nix
+++ shell.nix
@@ -1,25 +1,10 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell rec {
-  name = "parca-agent";
-
-  packages = with pkgs; [
-    clang_11
-    elfutils.dev
-    gnumake
-    go_1_19
-    kubectl
-    llvmPackages_14.llvm
-    minikube
-    docker-machine-kvm2
-    pkg-config
-    zlib.static
-  ] ++ (lib.optional stdenv.isLinux [ glibc.dev glibc.static ]);
-
-  shellHook = ''
-    export PATH="''${PROJECT_ROOT}/bin:''${PATH}"
-  '';
-
-  PROJECT_ROOT = builtins.toString ./.;
-  GOBIN = "${PROJECT_ROOT}/bin";
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
