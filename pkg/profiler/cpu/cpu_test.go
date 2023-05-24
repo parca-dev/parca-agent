812175c0237c34c3f5ceddfb6d9ec160e2dcd46a
Sumera Priyadarsini <sylphrenadin@gmail.com>
2023-05-24 21:01:42 +0530

    plij let frederic and javier be right

--- pkg/profiler/cpu/cpu_test.go
+++ pkg/profiler/cpu/cpu_test.go
@@ -33,11 +33,12 @@ import (
 // BPF program.
 func SetUpBpfProgram(t *testing.T) (*bpf.Module, error) {
 	t.Helper()
-	logger := logger.NewLogger("error", logger.LogFormatLogfmt, "parca-cpu-test")
+	logger := logger.NewLogger("debug", logger.LogFormatLogfmt, "parca-cpu-test")
 
 	memLock := uint64(1200 * 1024 * 1024) // ~1.2GiB
 	m, _, err := loadBpfProgram(logger, prometheus.NewRegistry(), true, true, true, memLock)
 	require.NoError(t, err)
+	require.NotNil(t, m)
 
 	return m, err
 }
