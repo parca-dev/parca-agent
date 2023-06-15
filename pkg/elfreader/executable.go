442f001c58c09fcb567d5f3972e717a0776c95c7
Kemal Akkoyun <kakkoyun@gmail.com>
2023-06-15 11:15:26 +0200

    Simplify base address calculation
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

---
+++ pkg/elfreader/executable.go
@@ -0,0 +1,199 @@
+// Copyright 2022-2023 The Parca Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elfreader
+
+import (
+	"debug/elf"
+	"fmt"
+)
+
+// IsASLRElegibleElf returns whether the elf executable could be elegible for
+// address space layout randomization (ASLR).
+//
+// Whether to enable ASLR for a process is decided in this kernel code
+// path (https://github.com/torvalds/linux/blob/v5.0/fs/binfmt_elf.c#L955).
+//
+// Note(javierhonduco): This check is a bit simplistic and might not work
+// for every case. We might want to check across multiple kernels. It probably
+// won't be correct for the dynamic loader itself. See link above.
+func IsASLRElegibleElf(elfFile *elf.File) bool {
+	return elfFile.FileHeader.Type == elf.ET_DYN
+}
+
+func IsASLRElegible(path string) (bool, error) {
+	elfFile, err := elf.Open(path)
+	if err != nil {
+		return false, fmt.Errorf("failed opening elf file with %w", err)
+	}
+	defer elfFile.Close()
+
+	return IsASLRElegibleElf(elfFile), nil
+}
+
+// Base determines the base address to subtract from virtual
+// address to get symbol table address. For an executable, the base
+// is 0. Otherwise, it's a shared library, and the base is the
+// address where the mapping starts. The kernel needs special handling.
+func Base(fh *elf.FileHeader, loadSegment *elf.ProgHeader, start, limit, offset uint64) (uint64, error) {
+	if start == 0 && offset == 0 && (limit == ^uint64(0) || limit == 0) {
+		// Some tools may introduce a fake mapping that spans the entire
+		// address space. Assume that the address has already been
+		// adjusted, so no additional base adjustment is necessary.
+		return 0, nil
+	}
+
+	//nolint:exhaustive
+	switch fh.Type {
+	case elf.ET_EXEC:
+		if loadSegment == nil {
+			// Assume fixed-address executable and so no adjustment.
+			return 0, nil
+		}
+		return start - offset + loadSegment.Off - loadSegment.Vaddr, nil
+	case elf.ET_REL:
+		if offset != 0 {
+			return 0, fmt.Errorf("don't know how to handle mapping.Offset")
+		}
+		return start, nil
+	case elf.ET_DYN:
+		// The process mapping information, start = start of virtual address range,
+		// and offset = offset in the executable file of the start address, tells us
+		// that a runtime virtual address x maps to a file offset
+		// fx = x - start + offset.
+		if loadSegment == nil {
+			return start - offset, nil
+		}
+
+		// The program header, if not nil, indicates the offset in the file where
+		// the executable segment is located (loadSegment.Off), and the base virtual
+		// address where the first byte of the segment is loaded
+		// (loadSegment.Vaddr). A file offset fx maps to a virtual (symbol) address
+		// sx = fx - loadSegment.Off + loadSegment.Vaddr.
+		//
+		// Thus, a runtime virtual address x maps to a symbol address
+		// sx = x - start + offset - loadSegment.Off + loadSegment.Vaddr.
+		return start - offset + loadSegment.Off - loadSegment.Vaddr, nil
+	}
+	return 0, fmt.Errorf("don't know how to handle FileHeader.Type %v", fh.Type)
+}
+
+// FindTextProgHeader finds the program segment header containing the .text
+// section or nil if the segment cannot be found.
+func FindTextProgHeader(f *elf.File) *elf.ProgHeader {
+	for _, s := range f.Sections {
+		if s.Name == ".text" {
+			// Find the LOAD segment containing the .text section.
+			for _, p := range f.Progs {
+				if p.Type == elf.PT_LOAD && p.Flags&elf.PF_X != 0 && s.Addr >= p.Vaddr && s.Addr < p.Vaddr+p.Memsz {
+					return &p.ProgHeader
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// ProgramHeadersForMapping returns the program segment headers that overlap
+// the runtime mapping with file offset mapOff and memory size mapSz. We skip
+// over segments zero file size because their file offset values are unreliable.
+// Even if overlapping, a segment is not selected if its aligned file offset is
+// greater than the mapping file offset, or if the mapping includes the last
+// page of the segment, but not the full segment and the mapping includes
+// additional pages after the segment end.
+// The function returns a slice of pointers to the headers in the input
+// slice, which are valid only while phdrs is not modified or discarded.
+func ProgramHeadersForMapping(phdrs []elf.ProgHeader, mapOff, mapSz uint64) []*elf.ProgHeader {
+	const (
+		// pageSize defines the virtual memory page size used by the loader. This
+		// value is dependent on the memory management unit of the CPU. The page
+		// size is 4KB virtually on all the architectures that we care about, so we
+		// define this metric as a constant. If we encounter architectures where
+		// page sie is not 4KB, we must try to guess the page size on the system
+		// where the profile was collected, possibly using the architecture
+		// specified in the ELF file header.
+		pageSize       = 4096
+		pageOffsetMask = pageSize - 1
+	)
+	mapLimit := mapOff + mapSz
+	var headers []*elf.ProgHeader
+	for i := range phdrs {
+		p := &phdrs[i]
+		// Skip over segments with zero file size. Their file offsets can have
+		// arbitrary values, see b/195427553.
+		if p.Filesz == 0 {
+			continue
+		}
+		segLimit := p.Off + p.Memsz
+		// The segment must overlap the mapping.
+		if p.Type == elf.PT_LOAD && mapOff < segLimit && p.Off < mapLimit {
+			// If the mapping offset is strictly less than the page aligned segment
+			// offset, then this mapping comes from a different segment, fixes
+			// b/179920361.
+			alignedSegOffset := uint64(0)
+			if p.Off > (p.Vaddr & pageOffsetMask) {
+				alignedSegOffset = p.Off - (p.Vaddr & pageOffsetMask)
+			}
+			if mapOff < alignedSegOffset {
+				continue
+			}
+			// If the mapping starts in the middle of the segment, it covers less than
+			// one page of the segment, and it extends at least one page past the
+			// segment, then this mapping comes from a different segment.
+			if mapOff > p.Off && (segLimit < mapOff+pageSize) && (mapLimit >= segLimit+pageSize) {
+				continue
+			}
+			headers = append(headers, p)
+		}
+	}
+	return headers
+}
+
+// HeaderForFileOffset attempts to identify a unique program header that
+// includes the given file offset. It returns an error if it cannot identify a
+// unique header.
+func HeaderForFileOffset(headers []*elf.ProgHeader, fileOffset uint64) (*elf.ProgHeader, error) {
+	var ph *elf.ProgHeader
+	for _, h := range headers {
+		if fileOffset >= h.Off && fileOffset < h.Off+h.Memsz {
+			if ph != nil {
+				// Assuming no other bugs, this can only happen if we have two or
+				// more small program segments that fit on the same page, and a
+				// segment other than the last one includes uninitialized data, or
+				// if the debug binary used for symbolization is stripped of some
+				// sections, so segment file sizes are smaller than memory sizes.
+				return nil, fmt.Errorf("found second program header (%#v) that matches file offset %x, first program header is %#v. Is this a stripped binary, or does the first program segment contain uninitialized data?", *h, fileOffset, *ph)
+			}
+			ph = h
+		}
+	}
+	if ph == nil {
+		return nil, fmt.Errorf("no program header matches file offset %x", fileOffset)
+	}
+	return ph, nil
+}
--- pkg/process/maps.go
+++ pkg/process/maps.go
@@ -30,63 +30,56 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/procfs"
 
-	"github.com/parca-dev/parca-agent/internal/pprof/elfexec"
+	"github.com/parca-dev/parca-agent/pkg/elfreader"
 	"github.com/parca-dev/parca-agent/pkg/objectfile"
 )
 
-var ErrBaseAddressCannotCalculated = errors.New("base address cannot be calculated")
-
-type AddressOutOfRangeError struct {
-	m    *Mapping
-	addr uint64
+type normalizationMetrics struct {
+	baseCalculationInitSuccess      prometheus.Counter
+	baseCalculationInitError        prometheus.Counter
+	baseCalculationNormalizeSuccess prometheus.Counter
+	baseCalculationNormalizeError   prometheus.Counter
 }
 
-func (e *AddressOutOfRangeError) Error() string {
-	return fmt.Sprintf("specified address %x is outside the mapping range [%x, %x] for ObjectFile %q", e.addr, e.m.StartAddr, e.m.EndAddr, e.m.AbsolutePath())
-}
-
-const (
-	lvObtainFD            = "obtain_fd"
-	lvOpenObjectfile      = "open_objectfile"
-	lvComputeKernelOffset = "compute_kernel_offset"
-)
-
-type mapMetrics struct {
-	initialized *prometheus.CounterVec
-	initErrors  *prometheus.CounterVec
-}
-
-func newMapMetrics(reg prometheus.Registerer) *mapMetrics {
-	m := &mapMetrics{
-		initialized: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-			Name: "parca_agent_mapping_initialized_total",
-			Help: "Total number of times a mapping was initialized.",
-		}, []string{"result"}),
-		initErrors: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-			Name: "parca_agent_mapping_initialization_errors_total",
-			Help: "Total number of times a mapping failed to init.",
-		}, []string{"type"}),
+func newNormalizationMetrics(reg prometheus.Registerer) *normalizationMetrics {
+	baseCalculation := promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "parca_agent_base_calculation_total",
+			Help: "Total number of base calculation attempts by stage.",
+		},
+		[]string{"stage", "result"},
+	)
+	m := &normalizationMetrics{
+		baseCalculationInitSuccess:      baseCalculation.WithLabelValues("init", "success"),
+		baseCalculationInitError:        baseCalculation.WithLabelValues("init", "error"),
+		baseCalculationNormalizeSuccess: baseCalculation.WithLabelValues("normalize", "success"),
+		baseCalculationNormalizeError:   baseCalculation.WithLabelValues("normalize", "error"),
 	}
-	m.initialized.WithLabelValues(lvSuccess)
-	m.initialized.WithLabelValues(lvFail)
-	m.initErrors.WithLabelValues(lvObtainFD)
-	m.initErrors.WithLabelValues(lvOpenObjectfile)
-	m.initErrors.WithLabelValues(lvComputeKernelOffset)
 	return m
 }
 
 type MapManager struct {
 	*procfs.FS
-	metrics *mapMetrics
+
+	// normalizationEnabled indicates whether the profiler has to
+	// normalize sampled addresses for PIC/PIE (position independent code/executable).
+	normalizationEnabled bool
+	normalizationMetrics *normalizationMetrics
 
 	objFilePool *objectfile.Pool
 }
 
-func NewMapManager(reg prometheus.Registerer, fs procfs.FS, objFilePool *objectfile.Pool) *MapManager {
+func NewMapManager(
+	reg prometheus.Registerer,
+	fs procfs.FS,
+	objFilePool *objectfile.Pool,
+	normalizationEnabled bool,
+) *MapManager {
 	return &MapManager{
-		FS:          &fs,
-		metrics:     newMapMetrics(reg),
-		objFilePool: objFilePool,
+		FS:                   &fs,
+		objFilePool:          objFilePool,
+		normalizationEnabled: normalizationEnabled,
+		normalizationMetrics: newNormalizationMetrics(reg),
 	}
 }
 
@@ -99,7 +92,7 @@ func (ms Mappings) ConvertToPprof() []*profile.Mapping {
 	// value aka unset).
 	i := uint64(1)
 	for _, m := range ms {
-		pprofMapping := m.ConvertToPprof()
+		pprofMapping := m.convertToPprof()
 		pprofMapping.ID = i
 		res = append(res, pprofMapping)
 		i++
@@ -138,12 +131,9 @@ func (mm *MapManager) MappingsForPID(pid int) (Mappings, error) {
 
 	res := make([]*Mapping, 0, len(maps))
 	var errs error
-	idx := 0
 	for _, m := range maps {
-		// TODO(kakkoyun): Try to parallelize this to minimize the race window.
 		mapping, err := mm.newUserMapping(m, pid)
 		if err != nil && !errors.Is(err, &elf.FormatError{}) {
-			mm.metrics.initialized.WithLabelValues(lvFail).Inc()
 			if os.IsNotExist(err) || errors.Is(err, fs.ErrNotExist) {
 				// High likely the file was unreachable due to short-lived process.
 				// A corresponding metrics should have recorded in newUserMapping.
@@ -157,9 +147,7 @@ func (mm *MapManager) MappingsForPID(pid int) (Mappings, error) {
 			// is not an ELF file.
 			continue
 		}
-		mm.metrics.initialized.WithLabelValues(lvSuccess).Inc()
 		res = append(res, mapping)
-		idx++
 	}
 	return res, errs
 }
@@ -191,18 +179,10 @@ type Mapping struct {
 	// This is needed for pprof conversion.
 	BuildID string
 
-	// Offset of kernel relocation symbol.
-	// Only defined for kernel images, nil otherwise. e. g. _stext.
-	//
-	// TODO: Remove or add InitOnce
-	kernelOffset *uint64
-
-	// Ensures the base, baseErr are computed once.
-	baseOnce *sync.Once
-	baseErr  error
 	base     uint64
-	// Hold on to the object file to prevent GC until base is computed.
-	objFile *objectfile.ObjectFile
+	baseOnce *sync.Once
+	baseSet  bool
+	baseErr  error
 
 	containsDebuginfoToUpload bool
 }
@@ -213,7 +193,6 @@ func (mm *MapManager) newUserMapping(pm *procfs.ProcMap, pid int) (*Mapping, err
 		mm:                        mm,
 		ProcMap:                   pm,
 		PID:                       pid,
-		baseOnce:                  &sync.Once{},
 		containsDebuginfoToUpload: true,
 	}
 
@@ -226,21 +205,30 @@ func (mm *MapManager) newUserMapping(pm *procfs.ProcMap, pid int) (*Mapping, err
 	if err != nil {
 		if !errors.Is(err, &elf.FormatError{}) {
 			m.containsDebuginfoToUpload = false
-			// We don't want to count these as errors. This just means the file
-			// is not an ELF file.
-			m.mm.metrics.initErrors.WithLabelValues(lvOpenObjectfile).Inc()
 		}
 		return nil, fmt.Errorf("failed to open mapped object file: %w", err)
 	}
 	defer obj.HoldOn()
 
-	if err := m.computeKernelOffset(obj); err != nil {
-		m.mm.metrics.initErrors.WithLabelValues(lvComputeKernelOffset).Inc()
-		return nil, fmt.Errorf("failed to compute kernel offset: %w", err)
+	ef, release, err := obj.ELF()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get ELF file: %w", err)
+	}
+	defer release()
+
+	m.BuildID = obj.BuildID
+
+	base, err := m.computeBaseWithoutAddr(ef)
+	if err == nil {
+		// If we can compute the base without addr, we can use it as the base.
+		m.base = base
+		m.baseSet = true
+		m.mm.normalizationMetrics.baseCalculationInitSuccess.Inc()
+	} else {
+		m.baseOnce = &sync.Once{}
+		m.mm.normalizationMetrics.baseCalculationInitError.Inc()
 	}
 
-	m.objFile = obj // Hold on to this until base is computed.
-	m.BuildID = obj.BuildID
 	return m, nil
 }
 
@@ -273,105 +261,14 @@ func (m *Mapping) AbsolutePath() string {
 	return path.Join("/proc", strconv.Itoa(m.PID), "/root", m.Pathname)
 }
 
-// kernelRelocationSymbol extracts kernel relocation symbol _text or _stext
-// for a main linux kernel mapping.
-// The mapping file can be [kernel.kallsyms]_text or [kernel.kallsyms]_stext.
-func kernelRelocationSymbol(mappingFile string) string {
-	const prefix = "[kernel.kallsyms]"
-	if !strings.HasPrefix(mappingFile, prefix) {
-		return ""
-	}
-	return mappingFile[len(prefix):]
-}
-
-// computeKernelOffset computes the offset of the kernel relocation symbol.
-func (m *Mapping) computeKernelOffset(obj *objectfile.ObjectFile) error {
-	defer obj.HoldOn()
-
-	if m == nil {
-		return nil
-	}
-
-	if m.kernelOffset != nil {
-		return nil
-	}
-
-	var (
-		relocationSymbol = kernelRelocationSymbol(m.AbsolutePath())
-		kernelOffset     *uint64
-		pageAligned      = func(addr uint64) bool { return addr%4096 == 0 }
-	)
-
-	if obj == nil {
-		panic("object file is nil")
-	}
-
-	ef, release, err := obj.ELF()
-	if err != nil {
-		return fmt.Errorf("failed to get ELF file: %w", err)
-	}
-	defer release()
-
-	if strings.Contains(m.AbsolutePath(), "vmlinux") ||
-		!pageAligned(uint64(m.StartAddr)) || !pageAligned(uint64(m.EndAddr)) || !pageAligned(uint64(m.Offset)) {
-		// Reading all Symbols is expensive, and we only rarely need it so
-		// we don't want to do it every time. But if _stext happens to be
-		// page-aligned but isn't the same as Vaddr, we would symbolize
-		// wrong. So if the name the addresses aren't page aligned, or if
-		// the name is "vmlinux" we read _stext. We can be wrong if: (1)
-		// someone passes a kernel path that doesn't contain "vmlinux" AND
-		// (2) _stext is page-aligned AND (3) _stext is not at Vaddr
-		symbols, err := ef.Symbols()
-		if err != nil && !errors.Is(err, elf.ErrNoSymbols) {
-			return err
-		}
-
-		// The kernel relocation symbol (the mapping start address) can be either
-		// _text or _stext. When profiles are generated by `perf`, which one was used is
-		// distinguished by the mapping name for the kernel image:
-		// '[kernel.kallsyms]_text' or '[kernel.kallsyms]_stext', respectively. If we haven't
-		// been able to parse it from the mapping, we default to _stext.
-		if relocationSymbol == "" {
-			relocationSymbol = "_stext"
-		}
-		for _, s := range symbols {
-			sym := s
-			if sym.Name == relocationSymbol {
-				kernelOffset = &sym.Value
-				break
-			}
-		}
-	}
-
-	// Check that we can compute a base for the binary. This may not be the
-	// correct base value, so we don't save it. We delay computing the actual base
-	// value until we have a sample address for this mapping, so that we can
-	// correctly identify the associated program segment that is needed to compute
-	// the base.
-	if _, err := elfexec.GetBase(
-		&ef.FileHeader,
-		elfexec.FindTextProgHeader(ef), kernelOffset,
-		uint64(m.StartAddr), uint64(m.EndAddr), uint64(m.Offset),
-	); err != nil {
-		return fmt.Errorf("could not identify base for %s: %w", m.AbsolutePath(), err)
-	}
-	m.kernelOffset = kernelOffset
-	return nil
-}
-
 // findProgramHeader returns the program segment that matches the current
 // mapping and the given address, or an error if it cannot find a unique program
 // header.
 func (m *Mapping) findProgramHeader(ef *elf.File, addr uint64) (*elf.ProgHeader, error) {
 	// For user space executables, we try to find the actual program segment that
 	// is associated with the given mapping. Skip this search if limit <= start.
-	// We cannot use just a check on the start address of the mapping to tell if
-	// it's a kernel / .ko module mapping, because with quipper address remapping
-	// enabled, the address would be in the lower half of the address space.
-
-	if m.kernelOffset != nil || m.StartAddr >= m.EndAddr || uint64(m.EndAddr) >= (uint64(1)<<63) {
-		// For the kernel, find the program segment that includes the .text section.
-		return elfexec.FindTextProgHeader(ef), nil
+	if m.StartAddr >= m.EndAddr || uint64(m.EndAddr) >= (uint64(1)<<63) {
+		return elfreader.FindTextProgHeader(ef), nil
 	}
 
 	// Fetch all the loadable segments.
@@ -384,11 +281,10 @@ func (m *Mapping) findProgramHeader(ef *elf.File, addr uint64) (*elf.ProgHeader,
 	// Some ELF files don't contain any loadable program segments, e.g. .ko
 	// kernel modules. It's not an error to have no header in such cases.
 	if len(phdrs) == 0 {
-		//nolint:nilnil
-		return nil, nil
+		return nil, nil //nolint:nilnil
 	}
 	// Get all program headers associated with the mapping.
-	headers := elfexec.ProgramHeadersForMapping(phdrs, uint64(m.Offset), uint64(m.EndAddr)-uint64(m.StartAddr))
+	headers := elfreader.ProgramHeadersForMapping(phdrs, uint64(m.Offset), uint64(m.EndAddr)-uint64(m.StartAddr))
 	if len(headers) == 0 {
 		return nil, errors.New("no program header matches mapping info")
 	}
@@ -398,60 +294,115 @@ func (m *Mapping) findProgramHeader(ef *elf.File, addr uint64) (*elf.ProgHeader,
 
 	// Use the file offset corresponding to the address to symbolize, to narrow
 	// down the header.
-	return elfexec.HeaderForFileOffset(headers, addr-uint64(m.StartAddr)+uint64(m.Offset))
+	return elfreader.HeaderForFileOffset(headers, addr-uint64(m.StartAddr)+uint64(m.Offset))
+}
+
+func (m *Mapping) computeBase(ef *elf.File, addr uint64) (uint64, error) {
+	if m == nil {
+		return 0, nil
+	}
+	if addr < uint64(m.StartAddr) || addr >= uint64(m.EndAddr) {
+		return 0, fmt.Errorf("specified address %x is outside the mapping range [%x, %x]", addr, m.StartAddr, m.EndAddr)
+	}
+
+	programHeader, err := m.findProgramHeader(ef, addr)
+	if err != nil {
+		return 0, fmt.Errorf("failed to find program header for mapping %#v: %w", m, err)
+	}
+
+	base, err := elfreader.Base(
+		&ef.FileHeader, programHeader,
+		uint64(m.StartAddr),
+		uint64(m.EndAddr),
+		uint64(m.Offset),
+	)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get base from ELF mapping %#v: %w", m, err)
+	}
+	return base, nil
+}
+
+func (m *Mapping) computeBaseWithoutAddr(ef *elf.File) (uint64, error) {
+	var programHeader *elf.ProgHeader
+	if m.StartAddr >= m.EndAddr || uint64(m.EndAddr) >= (uint64(1)<<63) {
+		programHeader = elfreader.FindTextProgHeader(ef)
+	}
+	if programHeader == nil {
+		return 0, errors.New("no program header matches mapping info")
+	}
+
+	base, err := elfreader.Base(
+		&ef.FileHeader, programHeader,
+		uint64(m.StartAddr),
+		uint64(m.EndAddr),
+		uint64(m.Offset),
+	)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get base from ELF mapping %#v: %w", m, err)
+	}
+	return base, nil
 }
 
 // Normalize converts the given address to the address relative to the start of the
 // object file.
 func (m *Mapping) Normalize(addr uint64) (uint64, error) {
-	if addr < uint64(m.StartAddr) || addr >= uint64(m.EndAddr) {
-		return 0, &AddressOutOfRangeError{m, addr}
+	if !m.mm.normalizationEnabled {
+		return addr, nil
 	}
-	m.baseOnce.Do(func() { m.computeBase(addr) })
-	if m.baseErr != nil {
-		return 0, errors.Join(m.baseErr, ErrBaseAddressCannotCalculated)
+
+	// Fast path: if the base is already set, we can just subtract it from the address.
+	if m.baseSet {
+		return addr - m.base, nil
 	}
-	return addr - m.base, nil
+
+	// Slow path: we need to compute the base using the received address to find the program header.
+	if m.baseErr == nil && m.baseOnce != nil {
+		m.baseOnce.Do(func() {
+			defer func() {
+				if m.baseErr != nil {
+					m.mm.normalizationMetrics.baseCalculationNormalizeError.Inc()
+				}
+			}()
+
+			obj, err := m.mm.objFilePool.Open(m.AbsolutePath())
+			if err != nil {
+				m.baseErr = fmt.Errorf("failed to open mapped object file: %w", err)
+				return
+			}
+			defer obj.HoldOn()
+
+			ef, release, err := obj.ELF()
+			if err != nil {
+				m.baseErr = fmt.Errorf("failed to get ELF file: %w", err)
+				return
+			}
+			defer release()
+
+			base, err := m.computeBase(ef, addr)
+			if err != nil {
+				m.baseErr = fmt.Errorf("failed to compute base: %w", err)
+				return
+			}
+
+			m.base = base
+			m.baseSet = true
+			m.mm.normalizationMetrics.baseCalculationNormalizeSuccess.Inc()
+		})
+		if m.baseErr != nil {
+			return 0, fmt.Errorf("failed to compute base: %w", m.baseErr)
+		}
+	}
+
+	if m.baseSet {
+		// Base calculated, we can now subtract it from the address.
+		return addr - m.base, nil
+	}
+	// Leave the address as is.
+	return addr, nil
 }
 
-// computeBase computes the relocation base for the given binary ObjectFile only if
-// the mapping field is set. It populates the base fields returns an error.
-func (m *Mapping) computeBase(addr uint64) {
-	if m.objFile == nil {
-		// This should never happen, but we check anyway.
-		m.baseErr = fmt.Errorf("object file is not set for mapping %q", m.AbsolutePath())
-		return
-	}
-
-	ef, release, err := m.objFile.ELF()
-	if err != nil {
-		m.baseErr = fmt.Errorf("failed to obtain ELF file from objectfile %q: %w", m.objFile.Path, err)
-		return
-	}
-	defer func() {
-		release()
-		m.objFile = nil
-	}()
-
-	ph, err := m.findProgramHeader(ef, addr)
-	if err != nil {
-		m.baseErr = fmt.Errorf("failed to find program header from objectfile %q, ELF mapping %#v, address %x: %w", m.objFile.Path, m, addr, err)
-		return
-	}
-
-	base, err := elfexec.GetBase(
-		&ef.FileHeader, ph, m.kernelOffset,
-		uint64(m.StartAddr), uint64(m.EndAddr), uint64(m.Offset),
-	)
-	if err != nil {
-		m.baseErr = fmt.Errorf("failed to get base from objectfile %q, ELF mapping %#v, address %x: %w", m.objFile.Path, m, addr, err)
-		return
-	}
-	m.base = base
-}
-
-// ConvertToPprof converts the Mapping to a pprof profile.Mapping.
-func (m *Mapping) ConvertToPprof() *profile.Mapping {
+// convertToPprof converts the Mapping to a pprof profile.Mapping.
+func (m *Mapping) convertToPprof() *profile.Mapping {
 	var (
 		buildID = m.BuildID
 		path    = m.Pathname
