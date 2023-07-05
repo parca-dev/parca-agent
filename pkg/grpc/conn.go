0eaf714cc95acfa6a4c5ed7723c0d0b4d36e5ee9
Kemal Akkoyun <kakkoyun@gmail.com>
2023-07-05 14:59:25 +0200

    Adjust parameters and document
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- pkg/grpc/conn.go
+++ pkg/grpc/conn.go
@@ -93,7 +93,18 @@ func Conn(logger log.Logger, reg prometheus.Registerer, tp trace.TracerProvider,
 		grpc.WithChainUnaryInterceptor(
 			timeout.UnaryClientInterceptor(unaryTimeout), // 5m by default.
 			retry.UnaryClientInterceptor(
-				retry.WithBackoff(retry.BackoffExponentialWithJitter(5*time.Second, 0.1)),
+				// Back-off with Jitter: scalar: 1s, jitterFraction: 0,1, 10 runs
+				// i: 1		t:969.91774ms		total:969.91774ms
+				// i: 2		t:1.914221005s		total:2.884138745s
+				// i: 3		t:3.788704363s		total:6.672843108s
+				// i: 4		t:8.285062088s		total:14.957905196s
+				// i: 5		t:14.480256611s		total:29.438161807s
+				// i: 6		t:32.586249789s		total:1m2.024411596s
+				// i: 7		t:1m6.755804584s	total:2m8.78021618s
+				// i: 8		t:2m3.116345957s	total:4m11.896562137s
+				// i: 9		t:4m3.895083732s	total:8m15.791645869s
+				// i: 10	t:9m19.350609671s	total:17m35.14225554s
+				retry.WithBackoff(retry.BackoffExponentialWithJitter(time.Second, 0.1)),
 				retry.WithMax(10),
 				// The passed in context has a `5m` timeout (see above), the whole invocation should finish within that time.
 				// However, by default all retried calls will use the parent context for their deadlines.
