0357be372370c03897ecbaa3c879c4a52cd0900c
Sumera Priyadarsini <sylphrenadin@gmail.com>
2023-11-23 16:48:18 +0530

    add kerneltests

--- kerneltest/vmtest.sh
+++ kerneltest/vmtest.sh
@@ -32,17 +32,20 @@ use_kernel() {
 
 github_start() {
     kernel_version=$1
-    [[ -z "${GITHUB_ACTIONS}" ]] || echo "::group:: running tests on kernel $kernel_version"
+    arch=$2
+    [[ -z "${GITHUB_ACTIONS}" ]] || echo "::group:: running tests on kernel $kernel_version arch: $arch"
 }
 
 github_end() {
     kernel_version=$1
-    [[ -z "${GITHUB_ACTIONS}" ]] || echo "::endgroup::"
+    arch=$2
+    [[ -z "${GITHUB_ACTIONS}" ]] || echo "::endgroup:: finished test on kernel $kernel_version arch: $arch"
 }
 
 test_info() {
     kernel_version=$1
-    cat <<EOT >"kerneltest/logs/vm_log_$kernel_version.txt"
+    arch=$2
+    cat <<EOT >"kerneltest/logs/vm_log_$kernel_version_$arch.txt"
 ============================================================
 - date: $(date)
 - git revision: $(git rev-parse HEAD)-$(git diff-index --quiet HEAD || echo dirty)
@@ -55,27 +58,29 @@ EOT
 vm_run() {
     kernel_version=$1
     memory=$2
+    arch=$3
     echo "running tests in qemu"
-    github_start "$kernel_version"
-    test_info "$kernel_version"
+    github_start "$kernel_version" "$arch"
+    test_info "$kernel_version" "$arch"
     # kernel.panic=-1 and -no-reboot ensures we won't get stuck on kernel panic.
     qemu-system-x86_64 -no-reboot -append 'printk.devkmsg=on kernel.panic=-1 crashkernel=256M' \
         -nographic -append "console=ttyS0" -m "$memory" -kernel "kerneltest/kernels/linux-$kernel_version.bz" \
         -initrd kerneltest/amd64/amd64-initramfs.cpio | tee -a "kerneltest/logs/vm_log_$kernel_version.txt"
-    github_end "$kernel_version"
+    github_end "$kernel_version" "$arch"
 }
 
 vm_run_arm() {
     kernel_version=$1
     memory=$2
+    arch=$3
     echo "running tests in qemu"
-    github_start "$kernel_version"
-    test_info "$kernel_version"
+    github_start "$kernel_version" "$arch"
+    test_info "$kernel_version" "$arch"
     # kernel.panic=-1 and -no-reboot ensures we won't get stuck on kernel panic.
     qemu-system-x86_64 -machine virt -no-reboot -append 'printk.devkmsg=on kernel.panic=-1 crashkernel=256M' \
         -nographic -append "console=ttyS0" -m "$memory" -kernel "kerneltest/kernels/linux-$kernel_version.bz" \
         -initrd kerneltest/arm64/arm64-initramfs.cpio | tee -a "kerneltest/logs/vm_log_$kernel_version.txt"
-    github_end "$kernel_version"
+    github_end "$kernel_version" "$arch"
 }
 
 did_test_pass() {
@@ -104,8 +109,8 @@ run_tests() {
         # Ensure that the adaptive unwind shard mechanism
         # works in memory constrained environments.
         if [[ "$kernel" == "5.4" ]]; then
-            vm_run "$kernel" "0.7G"
-            vm_run_arm "$kernel" "0.7G"
+            vm_run "$kernel" "0.7G" "x86"
+            vm_run_arm "$kernel" "0.7G" "aarch64"
         else
             vm_run "$kernel" "1.5G"
             vm_run_arm  "$kernel" "1.5G"
