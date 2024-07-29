804589da5ff7387c28eff1b6dd63ac08198f0886
Tommy Reilly <gnurizen@gmail.com>
2024-07-29 19:48:14 +0000

    fix arm build

--- pkg/runtime/lua/offsets_x86.go
+++ pkg/runtime/lua/offsets_x86.go
@@ -1,3 +1,5 @@
+//go:build amd64
+
 // Copyright 2024 The Parca Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,8 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-//go:build amd64
 
 package lua
 
