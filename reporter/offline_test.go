15d8bf6eaf2fa14ab1767383e37fa6a65dcddf1a
thorfour <me@thor-hansen.com>
2024-10-03 05:17:18 -0500

    dumb unit test

---
+++ reporter/offline_test.go
@@ -0,0 +1,28 @@
+package reporter
+
+import (
+	"errors"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/apache/arrow/go/v16/arrow/ipc"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Offline_ReadFile(t *testing.T) {
+	f, err := os.Open("/tmp/offline-data/019251da-d851-7cee-a4dd-385102cb1d9a.ipc")
+	require.NoError(t, err)
+
+	reader, err := ipc.NewReader(f)
+	require.NoError(t, err)
+
+	for {
+		batch, err := reader.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		require.NoError(t, err)
+		t.Log(batch)
+	}
+}
