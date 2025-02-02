478e98257aa11412a4901390cc1603299c03eec2
Ivan Babrou <github@ivan.computer>
2025-02-01 17:24:20 -0800

    Actually start tracing exporter
    Without it one gets `traces export: the client is shutdown` in the logs.

--- main.go
+++ main.go
@@ -439,6 +439,10 @@ func mainWithExitCode() flags.ExitCode {
 		go readTracePipe(mainCtx)
 	}
 
+	if err := exporter.Start(context.Background()); err != nil {
+		return flags.Failure("Failed to start OTLP exporter: %v", err)
+	}
+
 	// Block waiting for a signal to indicate the program should terminate
 	<-mainCtx.Done()
 
@@ -448,6 +452,13 @@ func mainWithExitCode() flags.ExitCode {
 		if err := grpcConn.Close(); err != nil {
 			log.Fatalf("Stopping connection of OTLP client client failed: %v", err)
 		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := exporter.Shutdown(ctx); err != nil {
+		log.Infof("Failed to stop exporter: %v", err)
 	}
 
 	log.Info("Exiting ...")
